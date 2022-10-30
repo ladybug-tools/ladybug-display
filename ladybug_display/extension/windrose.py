b51ac77f5ee7df19a07c8c91bba353d85c88dda0
Chris Mackey <chris@ladybug.tools>
2022-10-30 11:06:42 -0400

    fix(extension): Add a module for HourlyPlot to VisualizationSet

--- ladybug_display/extension/windrose.py
+++ ladybug_display/extension/windrose.py
@@ -1,3 +1,4 @@
+"""Method to draw a WindRose as a VisualizationSet."""
 from ladybug_geometry.geometry2d import Polyline2D
 from ladybug_geometry.geometry3d import Point3D, Plane, LineSegment3D, \
     Polyline3D, Mesh3D
@@ -9,12 +10,14 @@ from ladybug_display.visualization import VisualizationSet, AnalysisGeometry, \
 from ladybug_display.extension.compass import compass_to_vis_set
 
 
-def wind_rose_to_vis_set(windrose, z=0):
+def wind_rose_to_vis_set(windrose, z=0, frequency_labels=True):
     """Get a Ladybug WindRose represented as a VisualizationSet.
 
     Args:
         windrose: A Ladybug WindRose object.
-        z: A number for the Z-coordinate to be used in translation. (Default: 0)
+        z: A number for the Z-coordinate to be used in translation. (Default: 0).
+        frequency_labels: A boolean to note whether frequency labels should be
+            included in the output visualization. (Default: True).
 
     Returns:
         A VisualizationSet with the wind rose represented several ContextGeometries
@@ -57,15 +60,18 @@ def wind_rose_to_vis_set(windrose, z=0):
     freq_line = [Polyline3D.from_polyline2d(Polyline2D.from_polygon(poly), wr_pln)
                  for poly in windrose.frequency_lines[:-1]]
     dis_freq, freq_text = [], []
-    f_int = windrose.frequency_hours
-    txt_h = min((windrose.frequency_spacing_distance / 4, txt_h))
-    freqs = range(0, f_int * windrose.frequency_intervals_compass, f_int)
-    for i, (lin, val) in enumerate(zip(freq_line, freqs)):
+    for lin in freq_line:
         dis_freq.append(DisplayPolyline3D(lin, line_type='Dotted'))
-        if i % 2 == 0 and i != 0:
-            b_pln = Plane(o=lin.segments[0].midpoint)
-            d_txt = DisplayText3D(str(val), b_pln, txt_h, None, font, 'Center', 'Bottom')
-            freq_text.append(d_txt)
+    if frequency_labels:
+        f_int = windrose.frequency_hours
+        txt_h = min((windrose.frequency_spacing_distance / 4, txt_h))
+        freqs = range(0, f_int * windrose.frequency_intervals_compass, f_int)
+        for i, (lin, val) in enumerate(zip(freq_line, freqs)):
+            if i % 2 == 0 and i != 0:
+                b_pln = Plane(o=lin.segments[0].midpoint)
+                d_txt = DisplayText3D(str(val), b_pln, txt_h, None, font,
+                                      'Center', 'Bottom')
+                freq_text.append(d_txt)
     freq_geo = ContextGeometry('Frequency_Lines', dis_freq + freq_text)
     freq_geo.display_name = 'Frequency Lines'
     vis_set.add_geometry(freq_geo)
