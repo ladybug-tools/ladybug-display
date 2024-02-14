6ea02654b949935f6c3c26d890eb49d714c1139c
Chris Mackey <chris@ladybug.tools>
2024-02-14 11:33:27 -0800

    feat(extension): Add a method for ViewSphere.to_vis_set()

--- ladybug_display/_extend_ladybug.py
+++ ladybug_display/_extend_ladybug.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 # import the core ladybug modules
 from ladybug.compass import Compass
+from ladybug.viewsphere import ViewSphere
 from ladybug.sunpath import Sunpath
 from ladybug.windrose import WindRose
 from ladybug.windprofile import WindProfile
@@ -10,6 +11,7 @@ from ladybug.psychchart import PsychrometricChart
 
 # import the extension functions
 from .extension.compass import compass_to_vis_set
+from .extension.viewsphere import view_sphere_to_vis_set
 from .extension.sunpath import sunpath_to_vis_set
 from .extension.windrose import wind_rose_to_vis_set
 from .extension.windprofile import wind_profile_to_vis_set
@@ -19,6 +21,7 @@ from .extension.psychchart import psychrometric_chart_to_vis_set
 
 # inject the methods onto the classes
 Compass.to_vis_set = compass_to_vis_set
+ViewSphere.to_vis_set = view_sphere_to_vis_set
 Sunpath.to_vis_set = sunpath_to_vis_set
 WindRose.to_vis_set = wind_rose_to_vis_set
 WindProfile.to_vis_set = wind_profile_to_vis_set
